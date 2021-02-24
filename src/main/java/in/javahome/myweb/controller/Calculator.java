e871bfeb29f0744ef9aeb512ca3a67410739fecd
achrafbl <76996944+achrafbl@users.noreply.github.com>
2021-02-24 23:22:02 +0100

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -8,10 +8,10 @@ public class Calculator {
 	 * @param j
 	 * @return int
 	 */
-	public int add(int i, int j){
-		return i+j;
+	public int min(int i, int j){
+		return i-j;
 	}
-	public int multiply(int i, int j){
-		return i*j;
+	public int div(int i, int j){
+		return i/j;
 	}
 }
